51b665240a828a647c0115869eff495b1bfe7dd4
Muhammad hamza <hamzaofficial102@gmail.com>
2026-07-04 20:31:57 +0500

    send messages cfeature completed

---
+++ app/api/send-messages/route.ts
@@ -0,0 +1,38 @@
+import dbconnect from "@/lib/dbConnect";
+import userModel from "@/models/user";
+import { Message } from "@/models/user";
+
+export async function GET(request: Request) {
+    await dbconnect()
+    try {
+        const { username, content } = await request.json()
+        const user = await userModel.findOne({ userName: username })
+        if (!user) {
+            return Response.json(
+                { success: false, message: "User not found" },
+                { status: 404 }
+            );
+        }
+        if (!user.isAccepting) {
+            return Response.json(
+                { success: false, message: "User is not accepting messages!" },
+                { status: 403 }
+            );
+        }
+        const newMessage = { content, createdAt: new Date() }
+        user.messages.push(newMessage as Message)
+        await user.save()
+        return Response.json(
+            { success: true, message: "Message sent successfully!" },
+            { status: 200 }
+        );
+    } catch (error) {
+        console.log("error adding message", error);
+
+        return Response.json(
+            { success: false, message: "Internal server Error" },
+            { status: 500 }
+        );
+    }
+
+}
